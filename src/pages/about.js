f5ddbdcafae7192a8743e01d46e26b86f59d59ca
yatfu <55002354+yatfu@users.noreply.github.com>
2023-06-18 10:24:11 -1000

    added about placeholder

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,19 @@
 import * as React from "react"
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
+import Layout from '../components/Layout'
 
 export default function About() {
   return (
     <div>
-      <Navbar></Navbar>
-        <section id='section-about'>
+      <Layout>
+      <section id='section-about'>
+          <h1>Who We Are</h1>
+          <p>Duis ea laboris laboris esse nostrud ad in pariatur voluptate magna aliqua exercitation. Ullamco magna pariatur enim ea ea adipisicing ex exercitation occaecat quis. Eu deserunt cupidatat sint eu aute consequat labore fugiat mollit ea sunt reprehenderit. 
+          
+          Cillum nostrud do laboris cupidatat minim labore eu pariatur nostrud qui. Quis consequat irure ad anim culpa laborum non dolore reprehenderit sit fugiat tempor. Ut quis sunt ut aute ad proident ullamco eiusmod commodo enim.</p>
           
         </section>
-      <Footer></Footer>
+      </Layout>
+
     </div>
   );
 }
